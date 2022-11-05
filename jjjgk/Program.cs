a953eb3e835a5194439de0b3e32542b99d0a0e92
BCAS-Student31 <BCAS-Student31@DESKTOP-2AGPVOD>
2022-11-05 13:32:20 +0800

    Add project files.

---
+++ jjjgk/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace OCP_Mark_Zara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pencilTool = new PencilTool();
+            pencilTool.mouseOver();
+        }
+    }
+}
+
+
+public interface IPaintTool
+{
+    void mouseOver();
+
+    void mouseUp();
+
+}
+
+public class PencilTool:IPaintTool
+{
+   
+    public void mouseOver()
+    {
+        Console.WriteLine("Show pencil icon");
+    }
+
+    public void mouseUp()
+    {
+        Console.WriteLine("Draw a line.");
+    }
+}
+
+public class SquareTool : IPaintTool
+{
+    public void mouseOver()
+    {
+        Console.WriteLine("Show square icon");
+    }
+
+    public void mouseUp()
+    {
+        Console.WriteLine("Draw a square.");
+    }
+}
+
+public class EraserTool : IPaintTool
+{
+    public void mouseOver()
+    {
+        Console.WriteLine("Show an eraser icon");
+    }
+
+    public void mouseUp()
+    {
+        Console.WriteLine("Erase the selected object.");
+    }
+}
